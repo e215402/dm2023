f861caf89a6a396a696ad9e877554f9c368f9d01
e215402 <e215402@ie.u-ryukyu.ac.jp>
2023-04-20 15:30:02 +0900

    add 03

---
+++ chapter_one/03.py
@@ -0,0 +1,8 @@
+s = 'Now I need a drink, alcoholic of course, after the heavy lectures involving quantum mechanics.'
+
+# 不要な文字を削除
+words = s.replace(",","")
+words = words.replace(".","")
+# 単語に分解
+
+print([len(line) for line in words.split()])
